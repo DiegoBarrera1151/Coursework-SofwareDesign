48841bf86556f0859dea603b69e407b3b00ec63b
DiegoBarrera1151 <db1151@live.mdx.ac.uk>
2023-04-17 17:29:33 -0600

    Part 1 of the code

--- Main.java
+++ Main.java
@@ -3,10 +3,21 @@ import java.util.Arrays;
 public class MergeSort {
 
     public static void main(String[] args) {
-        String[] values = {"foo", "bar", "alice", "bob", "celine", "david"};
+        String[] values = {"hello", "zoo", "airplain", "bakery", "clean", "daisy","oliver","bear"};
         mergeSort(values, 0, values.length - 1);
         System.out.println("Result " + Arrays.toString(values));
     }
+
+    public static void mergeSort(String[] a, int from, int to) {
+        if (from == to) {
+            return;
+        }
+        int mid = (from + to) / 2;
+        // sort the first and the second half equally
+        mergeSort(a, from, mid);
+        mergeSort(a, mid + 1, to);
+        merge(a, from, mid, to);
+    }// end mergeSort part
 
 
 
