4661c865d996fd77c42eced09468167107a1bcc5
DiegoBarrera1151 <db1151@live.mdx.ac.uk>
2023-04-17 17:20:06 -0600

    Creating a inside test
    For me to check if the code works while i am coding

--- Main.java
+++ Main.java
@@ -2,6 +2,11 @@ import java.util.Arrays;
 
 public class MergeSort {
 
+    public static void main(String[] args) {
+        String[] values = {"foo", "bar", "alice", "bob", "celine", "david"};
+        mergeSort(values, 0, values.length - 1);
+        System.out.println("Result " + Arrays.toString(values));
+    }
 
 
 
