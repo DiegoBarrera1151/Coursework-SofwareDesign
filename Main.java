7076b871d82b8cb0daf8c89dc61140a6cc982a76
DiegoBarrera1151 <db1151@live.mdx.ac.uk>
2023-04-17 17:38:37 -0600

    Second part (safe 1)

--- Main.java
+++ Main.java
@@ -17,8 +17,28 @@ public class MergeSort {
         mergeSort(a, from, mid);
         mergeSort(a, mid + 1, to);
         merge(a, from, mid, to);
-    }// end mergeSort part
+    // end mergeSort part
+    }
 
+    public static void merge(String[] a, int from, int mid, int to) {
+        int n = to - from;       // size of the list to be merged
+        String[] b = new String[n];   // merge both halves into a temporary b
+        int i1 = from;               // next element in the first list
+        int i2 = mid;            // next element in the second list
+        int j = 0;                   // next open position in b
+
+        // moving the smaller i1 i2 into b
+        while (i1 <= mid && i2 <= to) {
+            if (a[i1].compareTo(a[i2]) < 0) {
+                b[j] = a[i1];
+                i1++;
+            }
+            else {
+                b[j] = a[i2];
+                i2++;
+            }
+            j++;
+        }
 
 
     
